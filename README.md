08b8def7b35bc115f8c4a8c85182ddb7623458e6
Pascal ECHEMANN <developers@flashapi.org>
2018-04-29 16:28:29 +0200

    Minor Update
    Fixing some misspelling. Adding links to the new JEC website.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JEC GlassCat Metadata Project
 
-JEC GlassCat Metadata is a [JavaScript Entreprise Container][jec-url] project which provides utility tools for working with GlassCat server metadata.
+JEC GlassCat Metadata is a [JavaScript Enterprise Container][jec-url] project which provides utility tools for working with GlassCat server metadata.
 
 [![][jec-logo]][jec-url]
 
@@ -82,5 +82,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ```
 
-[jec-url]: https://github.com/pechemann/JEC
+[jec-url]: http://jecproject.org
 [jec-logo]: https://raw.githubusercontent.com/pechemann/JEC/master/assets/jec-logos/jec-logo.png
--- types/jec-glasscat-metadata.d.ts
+++ types/jec-glasscat-metadata.d.ts
@@ -2,11 +2,52 @@
  * JEC GlassCat Metadata Module
  * Copyright(c) 2017-2018 Pascal ECHEMANN
  * Apache 2.0 Licensed
- * This is a part of the JEC Projects: <https://github.com/pechemann/JEC>
+ * This is a part of the JEC projects: <http://jecproject.org>
  */
 
 declare module "jec-glasscat-metadata" {
 
 import { Singleton } from "jec-commons";
 
-export class GlassCatMetadataContext implements Singleton {    constructor();    private static INSTANCE;    private static _locked;    static getInstance(): GlassCatMetadataContext;    private initObj();    private _version;    private _id;    getVersion(): string;    setVersion(version: string): void;    getId(): string;}export class GlassCatMetadataValidationError extends Error {    constructor(message: string);}export class GlassCatMetadata {    constructor();    id: string;    creation: number;    version: string;}export class GlassCatMetadataGenerator {    constructor(version: string);    private _version;    private initObj(version);    getVersion(): string;    generate(): GlassCatMetadata;    generateFile(): string;}export class GlassCatMetadataFactory {    constructor();    create(): GlassCatMetadata;}export class GlassCatMetadataParser {    constructor();    parse(config: any): GlassCatMetadata;}export class GlassCatMetadataValidator {    constructor();    validate(metadata: GlassCatMetadata): boolean;}}
+export class GlassCatMetadataContext implements Singleton {
+    constructor();
+    private static INSTANCE;
+    private static _locked;
+    static getInstance(): GlassCatMetadataContext;
+    private initObj();
+    private _version;
+    private _id;
+    getVersion(): string;
+    setVersion(version: string): void;
+    getId(): string;
+}
+export class GlassCatMetadataValidationError extends Error {
+    constructor(message: string);
+}
+export class GlassCatMetadata {
+    constructor();
+    id: string;
+    creation: number;
+    version: string;
+}
+export class GlassCatMetadataGenerator {
+    constructor(version: string);
+    private _version;
+    private initObj(version);
+    getVersion(): string;
+    generate(): GlassCatMetadata;
+    generateFile(): string;
+}
+export class GlassCatMetadataFactory {
+    constructor();
+    create(): GlassCatMetadata;
+}
+export class GlassCatMetadataParser {
+    constructor();
+    parse(config: any): GlassCatMetadata;
+}
+export class GlassCatMetadataValidator {
+    constructor();
+    validate(metadata: GlassCatMetadata): boolean;
+}
+}
